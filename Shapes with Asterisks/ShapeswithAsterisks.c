03802b57dc94a06a80d374432dfcb052ded828ca
Canniestbadge <Canniestbadge@DESKTOP-NQ8D2SG>
2023-10-01 14:50:59 -0400

    Add project files.

---
+++ Shapes with Asterisks/ShapeswithAsterisks.c
@@ -0,0 +1,32 @@
+// Programmer:		Thecanniestbadge
+// Date:			5/22/2023
+// Program Name:	Shapes with Asterisks
+// Description:		This program runs with asterisks and creates a rectangle, oval, arrow, and diamond using asterisks
+
+#define _CRT_SECURE_NO_WARNINGS // Disable warnings (and errors) when using non-secure versions of printf, scanf, strcpy, etc.
+#include <stdio.h> // Needed for working with printf and scanf
+
+int main(void)
+{
+	printf("*********  |     ***     |      *      |      *       \n");
+	printf("*       *  |   *     *   |     ***     |     * *     \n");
+	printf("*       *  |  *       *  |    *****    |    *   *    \n");
+	printf("*       *  |  *       *  |      *      |   *     *   \n");
+	printf("*       *  |  *       *  |      *      |  *       *  \n");
+	printf("*       *  |  *       *  |      *      |   *     *   \n");
+	printf("*       *  |  *       *  |      *      |    *   *    \n");
+	printf("*       *  |   *     *   |      *      |     * *     \n");
+	printf("*********  |     ***     |      *      |      *      \n");
+	printf("\n");
+	printf("  *             ***     *               *   ******   \n");
+	printf("  *           *     *    *             *    *        \n");
+	printf("  *          *       *    *           *     *        \n");
+	printf("  *          *       *     *         *      *        \n");
+	printf("  *          *       *      *       *       ******   \n");
+	printf("  *          *       *       *     *        *        \n");
+	printf("  *          *       *        *   *         *        \n");
+	printf("  *           *     *          * *          *        \n");
+	printf("  *******       ***             *           ******   \n");
+	printf("\n");
+	return 0;
+} // end main()
